4909bffe932dae6d40ec3a969d133a922d8c4892
yying666 <yyingq06@gmail.com>
2021-12-19 03:37:24 +0000

    commit1

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+    let user_name="";
+    let gender="";
+
+    async function getAge() {
+        fetch('https://api.genderize.io/?name=' + user_name)
+	        .then((response) => response.json())
+	        .then((data) => {
+                user_name=data.name;
+                gender=data.gender;
+	});
+
+    }
+</script>
+<h1>Guess Age</h1>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="name">Name:</label>
+    <input id="user_name" bind:value={user_name} type="text" />
+    <button type="submit">Check</button>
+</form>
+
+<p>{gender}</p>
